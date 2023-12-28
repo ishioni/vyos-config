2ef36dcb06edd894113c0e41641fe2827e4aff9e
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-12-28 10:36:55 +0100

    fix: whoops, VLANs actually start from 1, not 0, so adjust everything

--- config-parts/service-dns_server.sh
+++ config-parts/service-dns_server.sh
@@ -1,4 +1,4 @@
 #!/bin/vbash
 
-set service dns forwarding allow-from '10.1.0.0/24'
-set service dns forwarding listen-address '10.1.0.1'
+set service dns forwarding allow-from '10.1.1.0/24'
+set service dns forwarding listen-address '10.1.1.1'
