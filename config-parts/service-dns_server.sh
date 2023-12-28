55183ff88d320f5065cc84c85bd1c0f872c98d06
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-12-28 23:27:40 +0100

    set dns forwarder

--- config-parts/service-dns_server.sh
+++ config-parts/service-dns_server.sh
@@ -1,10 +1,21 @@
 #!/bin/vbash
 
+# NETWORK
 set service dns forwarding allow-from '10.1.1.0/24'
 set service dns forwarding listen-address '10.1.1.1'
 
-set service dns forwarding allow-from '192.168.2.0/24'
-set service dns forwarding listen-address '192.168.2.1'
+# SERVERS
+set service dns forwarding allow-from '10.1.2.0/24'
+set service dns forwarding listen-address '10.1.2.1'
 
-set service dns forwarding allow-from '192.168.4.0/24'
-set service dns forwarding listen-address '192.168.4.1'
+# IOT
+set service dns forwarding allow-from '10.1.3.0/24'
+set service dns forwarding listen-address '10.1.3.1'
+
+# KUBERNETES
+set service dns forwarding allow-from '10.1.4.0/24'
+set service dns forwarding listen-address '10.1.4.1'
+
+# TRUSTED
+set service dns forwarding allow-from '10.1.5.0/24'
+set service dns forwarding listen-address '10.1.5.1'
