b967e26b0ab12a2d2792acab7cca035a3aa048b8
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-12-29 00:00:24 +0100

    chore: move out iface defitions

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -1,19 +1,8 @@
 #!/bin/vbash
 
-# WAN
-set interfaces ethernet eth0 description 'POE - Fiber'
+# Interface definitions
+set interfaces ethernet eth0 description 'POE'
 set interfaces ethernet eth0 hw-id '00:f0:da:ef:0a:d8'
-set interfaces ethernet eth0 address dhcp
-
-# Not until switchover
-# set interfaces ethernet eth0 vif 35 descriptioniption 'Internet'
-
-# set interfaces pppoe pppoe0 description 'WAN - Fiber'
-# set interfaces pppoe pppoe0 source-interface 'eth0.35'
-# set interfaces pppoe pppoe0 authentication user "${SECRET_ISP_AUTH_USER}"
-# set interfaces pppoe pppoe0 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
-
-# Unused
 set interfaces ethernet eth1 description '2.5GbE'
 set interfaces ethernet eth1 hw-id '00:f0:da:ef:0a:d5'
 set interfaces ethernet eth2 description '2.5GbE'
@@ -24,6 +13,17 @@ set interfaces ethernet eth5 description 'SFP1'
 set interfaces ethernet eth5 hw-id '50:6b:4b:0c:1d:d9'
 set interfaces ethernet eth6 description 'SFP2'
 set interfaces ethernet eth6 hw-id '50:6b:4b:0c:1d:d8'
+
+# WAN
+set interfaces ethernet eth0 address dhcp
+
+# Not until switchover
+# set interfaces ethernet eth0 vif 35 descriptioniption 'Internet'
+
+# set interfaces pppoe pppoe0 description 'WAN - Fiber'
+# set interfaces pppoe pppoe0 source-interface 'eth0.35'
+# set interfaces pppoe pppoe0 authentication user "${SECRET_ISP_AUTH_USER}"
+# set interfaces pppoe pppoe0 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
 
 # BRIDGE
 set interfaces bridge br0 description 'NETWORK'
