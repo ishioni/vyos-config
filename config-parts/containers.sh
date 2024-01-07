469f61bdf534178c64dcc95ba20cf00fb2102634
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-01-07 13:25:47 +0100

    fix: disable logspam for now

--- config-parts/containers.sh
+++ config-parts/containers.sh
@@ -34,36 +34,36 @@ set container name dnsdist volume config mode 'ro'
 # set container name bind volume cache mode 'rw'
 
 # unifi
-set container name unifi environment TZ value 'Europe/Warsaw'
-set container name unifi environment BIND_PRIV value 'true'
-set container name unifi environment RUNAS_UID0 value 'false'
-set container name unifi environment PGID value '999'
-set container name unifi environment PUID value '999'
-set container name unifi image 'ghcr.io/goofball222/unifi:8.0.24'
-set container name unifi memory '0'
-set container name unifi network services address '10.5.0.10'
-set container name unifi restart 'on-failure'
-set container name unifi shared-memory '0'
-set container name unifi volume cert source '/config/containers/unifi/cert'
-set container name unifi volume cert destination '/usr/lib/unifi/cert'
-set container name unifi volume cert mode 'rw'
-set container name unifi volume data source '/config/containers/unifi/data'
-set container name unifi volume data destination '/usr/lib/unifi/data'
-set container name unifi volume data mode 'rw'
-set container name unifi volume logs source '/config/containers/unifi/logs'
-set container name unifi volume logs destination '/usr/lib/unifi/logs'
-set container name unifi volume logs mode 'rw'
-set container name unifi cap-add 'net-bind-service'
+# set container name unifi environment TZ value 'Europe/Warsaw'
+# set container name unifi environment BIND_PRIV value 'true'
+# set container name unifi environment RUNAS_UID0 value 'false'
+# set container name unifi environment PGID value '999'
+# set container name unifi environment PUID value '999'
+# set container name unifi image 'ghcr.io/goofball222/unifi:8.0.24'
+# set container name unifi memory '0'
+# set container name unifi network services address '10.5.0.10'
+# set container name unifi restart 'on-failure'
+# set container name unifi shared-memory '0'
+# set container name unifi volume cert source '/config/containers/unifi/cert'
+# set container name unifi volume cert destination '/usr/lib/unifi/cert'
+# set container name unifi volume cert mode 'rw'
+# set container name unifi volume data source '/config/containers/unifi/data'
+# set container name unifi volume data destination '/usr/lib/unifi/data'
+# set container name unifi volume data mode 'rw'
+# set container name unifi volume logs source '/config/containers/unifi/logs'
+# set container name unifi volume logs destination '/usr/lib/unifi/logs'
+# set container name unifi volume logs mode 'rw'
+# set container name unifi cap-add 'net-bind-service'
 
 # cloudflare-ddns
-set container name cloudflare-ddns allow-host-networks
-set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
-set container name cloudflare-ddns environment DOMAINS value "${SECRET_DOMAIN}"
-set container name cloudflare-ddns environment IP6_PROVIDER value "none"
-set container name cloudflare-ddns environment TZ value 'Europe/Warsaw'
-set container name cloudflare-ddns environment PGID value "1000"
-set container name cloudflare-ddns environment PUID value "1000"
-set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.11.0'
-set container name cloudflare-ddns memory '0'
-set container name cloudflare-ddns restart 'on-failure'
-set container name cloudflare-ddns shared-memory '0'
+# set container name cloudflare-ddns allow-host-networks
+# set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
+# set container name cloudflare-ddns environment DOMAINS value "${SECRET_DOMAIN}"
+# set container name cloudflare-ddns environment IP6_PROVIDER value "none"
+# set container name cloudflare-ddns environment TZ value 'Europe/Warsaw'
+# set container name cloudflare-ddns environment PGID value "1000"
+# set container name cloudflare-ddns environment PUID value "1000"
+# set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.11.0'
+# set container name cloudflare-ddns memory '0'
+# set container name cloudflare-ddns restart 'on-failure'
+# set container name cloudflare-ddns shared-memory '0'
