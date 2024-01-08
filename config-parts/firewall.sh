a3c813d567b161403c6b359f2589493b2393c41c
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-01-08 22:28:53 +0100

    feat: add some mtu magic

--- config-parts/firewall.sh
+++ config-parts/firewall.sh
@@ -5,6 +5,7 @@ set firewall global-options state-policy established action 'accept'
 set firewall global-options state-policy related action 'accept'
 set firewall global-options all-ping enable
 set firewall global-options broadcast-ping disable
+set firewall global-options source-validation strict
 
 # Empty WAN address group for NAT - this will be filled on boot by the dhcp hook
 set firewall group address-group ADDRv4_eth0
