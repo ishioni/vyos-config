3d56963cf5dc95e35dda2f9c1c098ef7c851f5d8
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-01-14 11:56:39 +0100

    fix: typos

--- config-parts/firewall-ipv6.sh
+++ config-parts/firewall-ipv6.sh
@@ -2,20 +2,20 @@
 
 set firewall ipv6 forward filter rule 10 action jump
 set firewall ipv6 forward filter rule 10 inbound-interface name pppoe1
-set firewall ipv6 forward filter rule 10 jump-taget WAN_IN
+set firewall ipv6 forward filter rule 10 jump-target WAN_IN
 
 set firewall ipv6 input filter rule 10 action jump
 set firewall ipv6 input filter rule 10 inbound-interface name pppoe1
-set firewall ipv6 input filter rule 10 jump-taget WAN_IN
+set firewall ipv6 input filter rule 10 jump-target WAN_IN
 
-set firewall ipv6 name WAN_IN default action drop
+set firewall ipv6 name WAN_IN default-action drop
 set firewall ipv6 name WAN_IN rule 10 action accept
 set firewall ipv6 name WAN_IN rule 10 state established
 set firewall ipv6 name WAN_IN rule 10 state related
 set firewall ipv6 name WAN_IN rule 20 action accept
 set firewall ipv6 name WAN_IN rule 20 protocol icmpv6
 
-set firewall ipv6 name WAN_LOCAL default action drop
+set firewall ipv6 name WAN_LOCAL default-action drop
 set firewall ipv6 name WAN_LOCAL rule 10 action accept
 set firewall ipv6 name WAN_LOCAL rule 10 state established
 set firewall ipv6 name WAN_LOCAL rule 10 state related
