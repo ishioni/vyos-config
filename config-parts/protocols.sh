bf5a76b548d629f2e7ded5b2a7f405710696effb
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-01-16 11:11:34 +0100

    chore: add new talos bgp

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -3,6 +3,26 @@
 set protocols bgp system-as '64500'
 set protocols bgp parameters router-id '10.1.4.1'
 
+# New Talos BGP
+set protocols bgp neighbor 10.1.2.30 description 'master-0'
+set protocols bgp neighbor 10.1.2.30 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.2.30 remote-as '65501'
+set protocols bgp neighbor 10.1.2.31 description 'master-1'
+set protocols bgp neighbor 10.1.2.31 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.2.31 remote-as '65501'
+set protocols bgp neighbor 10.1.2.32 description 'master-2'
+set protocols bgp neighbor 10.1.2.32 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.2.32 remote-as '65501'
+set protocols bgp neighbor 10.1.2.40 description 'worker-0'
+set protocols bgp neighbor 10.1.2.40 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.2.40 remote-as '65501'
+set protocols bgp neighbor 10.1.2.41 description 'worker-1'
+set protocols bgp neighbor 10.1.2.41 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.2.41 remote-as '65501'
+set protocols bgp neighbor 10.1.2.42 description 'worker-2'
+set protocols bgp neighbor 10.1.2.42 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.2.42 remote-as '65501'
+
 # Talos BGP
 set protocols bgp neighbor 10.1.4.10 description 'master-0'
 set protocols bgp neighbor 10.1.4.10 address-family ipv4-unicast
