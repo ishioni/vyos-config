b07079f422f7542644b3ef9402a0855bfe6cc482
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-02-02 23:31:08 +0100

    no longer need to bridge 2 switches

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -40,35 +40,22 @@ set interfaces pppoe pppoe1 source-interface 'eth0.35'
 set interfaces pppoe pppoe1 authentication user "${SECRET_ISP_AUTH_USER}/ipv6"
 set interfaces pppoe pppoe1 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
 set interfaces pppoe pppoe1 ipv6 address 'autoconf'
-set interfaces pppoe pppoe1 dhcpv6-options pd 0 interface br0.5 address '1'
-set interfaces pppoe pppoe1 dhcpv6-options pd 0 interface br0.5 sla-id '0'
+set interfaces pppoe pppoe1 dhcpv6-options pd 0 interface eth5.5 address '1'
+set interfaces pppoe pppoe1 dhcpv6-options pd 0 interface eth5.5 sla-id '0'
 set interfaces pppoe pppoe1 dhcpv6-options pd 0 length '56'
 set interfaces pppoe pppoe1 mtu '1500'
 set interfaces pppoe pppoe1 mru '1500'
 set interfaces pppoe pppoe1 'no-default-route'
 set interfaces pppoe pppoe1 'disable' #Broken!
 
-# BRIDGE
-set interfaces bridge br0 description 'NETWORK'
-set interfaces bridge br0 address '10.1.1.1/24'
-set interfaces bridge br0 'enable-vlan'
-set interfaces bridge br0 member interface eth4 native-vlan '1'
-set interfaces bridge br0 member interface eth4 allowed-vlan '1-100'
-set interfaces bridge br0 member interface eth5 native-vlan '1'
-set interfaces bridge br0 member interface eth5 allowed-vlan '1-100'
-set interfaces bridge br0 member interface eth6 native-vlan '1'
-set interfaces bridge br0 member interface eth6 allowed-vlan '1-100'
-set interfaces bridge br0 'stp'
-set interfaces bridge br0 forwarding-delay '5'
-set interfaces bridge br0 igmp 'snooping'
-
 # LAN
-set interfaces bridge br0 vif 2 address '10.1.2.1/24'
-set interfaces bridge br0 vif 2 description 'SERVERS'
-set interfaces bridge br0 vif 3 address '10.1.3.1/24'
-set interfaces bridge br0 vif 3 description 'IOT'
-set interfaces bridge br0 vif 5 address '10.1.5.1/24'
-set interfaces bridge br0 vif 5 description 'TRUSTED'
+set interfaces ethernet eth5 address '10.1.1.1/24'
+set interfaces ethernet eth5 vif 2 address '10.1.2.1/24'
+set interfaces ethernet eth5 vif 2 description 'SERVERS'
+set interfaces ethernet eth5 vif 3 address '10.1.3.1/24'
+set interfaces ethernet eth5 vif 3 description 'IOT'
+set interfaces ethernet eth5 vif 5 address '10.1.5.1/24'
+set interfaces ethernet eth5 vif 5 description 'TRUSTED'
 
 # Wireguard
 set interfaces wireguard wg0 address '10.254.254.1/24'
