f7e6359ae3bc105ad13710fee3c5ecb3bee905ab
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-03-23 15:27:20 +0100

    fix

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -12,7 +12,7 @@ set interfaces ethernet eth4 hw-id 'e4:3a:6e:5a:f8:09'
 
 set interfaces ethernet eth6 description 'WAN'
 set interfaces ethernet eth6 address 'dhcp'
-set interfaces ethernet eth6 hw-id 'e4:3a:6e:5a:f8:0a'
+set interfaces ethernet eth6 hw-id '50:6b:4b:0c:1d:d8'
 
 set system login user vyos authentication public-keys scotte key 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDP1JMwwIE7/qpqLNOvIcYNy6CHhfR8S/Tm0ZCBFchcsPuvtQ2yuqjmi6DGaDiUzV2ln8tFzBVhi+eOor9r5l/XwK0wcNpuBbdNf0/C0z6SklEKZctU5sCFvEIw4V4WfNctChrarPCfZo7lae/7PJKtYQDuqwC0KWY2I43+kPPkR0o+sRYcMdvYgBHcNfUQNcXoO0nWlMcaEocmcFBq82E7RI8uY5RR6liF/VvpIj5C9FviTd7IIFdhVy+w6p7QJr/kUQAQCYF2sVrAH+ZqVVUh18LhaA0SM4mqnJyaCqKfdl8orufRaI61uxS70RlnJH0WYALejOqtx7IBMJGOdTM0ZlCYYpfqqUrRvbYQdiMlfXSCoMk8r3ldSY+FLw3FBMnOzUK35Srio1g6xoYsRChQbLZiJKDBRcGNghmiLuT3EsGF37+hjOOtKWLXXSnPZQKQckc5O1spSW4oR8Ij4JXfDyKL0n5H+MPn8oThK+jePTmCPLKMUo9OpFAtz/maZ8z8mAkHpdVt7mjL3D1sEGkIbo6XDjICfFEjLbnVJhKWAXluuAkzL9Bp52lkop8V4ALk5oTVe/c52oJQhiD6XVjwjJJ0DrvGScLhDzZARpd1d2eaGE4fbow8NgkkW5lpaXNvW0bN6L/+7N4nHLPWD9WKRU6Lee2FXk0C5Gnn1QB0zQ=='
 set system login user vyos authentication public-keys scotte type 'ssh-rsa'
