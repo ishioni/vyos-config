f8bc7142e29d9b19ff8e73909c27f41d0798d13b
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-08-15 13:19:25 +0200

    adjust firewall too

--- config-parts/0-ipv4-firewall-functions.sh
+++ config-parts/0-ipv4-firewall-functions.sh
@@ -122,7 +122,7 @@ function allow-traffic-http {
   zone=$1
   set firewall ipv4 name $zone rule 200 description 'Rule: accept http'
   set firewall ipv4 name $zone rule 200 action 'accept'
-  set firewall ipv4 name $zone rule 200 destination address "10.84.2.2"
+  set firewall ipv4 name $zone rule 200 destination address "10.84.2.1"
   set firewall ipv4 name $zone rule 200 destination port 'http'
   set firewall ipv4 name $zone rule 200 protocol 'tcp'
 }
@@ -131,7 +131,7 @@ function allow-traffic-https {
   zone=$1
   set firewall ipv4 name $zone rule 210 description 'Rule: accept https'
   set firewall ipv4 name $zone rule 210 action 'accept'
-  set firewall ipv4 name $zone rule 210 destination address "10.84.2.2"
+  set firewall ipv4 name $zone rule 210 destination address "10.84.2.1"
   set firewall ipv4 name $zone rule 210 destination port 'https'
   set firewall ipv4 name $zone rule 210 protocol 'tcp'
 }
@@ -140,8 +140,17 @@ function allow-traffic-torrent {
   zone=$1
   set firewall ipv4 name $zone rule 220 description 'Rule: accept torrent'
   set firewall ipv4 name $zone rule 220 action 'accept'
-  set firewall ipv4 name $zone rule 220 destination address "10.84.2.4"
+  set firewall ipv4 name $zone rule 220 destination address "10.84.2.2"
   set firewall ipv4 name $zone rule 220 destination group port-group 'torrent'
+  set firewall ipv4 name $zone rule 220 protocol 'tcp'
+}
+
+function allow-traffic-minecraft {
+  zone=$1
+  set firewall ipv4 name $zone rule 220 description 'Rule: accept torrent'
+  set firewall ipv4 name $zone rule 220 action 'accept'
+  set firewall ipv4 name $zone rule 220 destination address "10.84.2.3"
+  set firewall ipv4 name $zone rule 220 destination group port-group 'minecraft'
   set firewall ipv4 name $zone rule 220 protocol 'tcp'
 }
 
